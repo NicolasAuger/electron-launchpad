b27d73a6db1d92d35b4783f53da7f53e21a804df
Simon Deflesschouwer <simon.deflesschouwer@mmibordeaux.com>
2025-12-19 15:45:58 +0100

    feat: working react app

---
+++ electron/main.js
@@ -0,0 +1,45 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow;
+
+function createWindow() {
+    const isDev = !app.isPackaged;
+
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, '../electron/preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+        },
+    });
+
+    if (isDev) {
+        const devUrl = process.env.VITE_DEV_SERVER_URL || 'http://localhost:5173';
+        mainWindow.loadURL(devUrl);
+        mainWindow.webContents.openDevTools();
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
