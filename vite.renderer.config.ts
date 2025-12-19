bc70401dd3f204a816f1c550bbc6d8f6d1ad2748
Ugo Stephant <ugo@poool.fr>
2025-12-19 16:32:42 +0100

    chore: add react

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   root: './src/renderer',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   build: {
     outDir: '../../.vite/renderer/main_window',
   },
